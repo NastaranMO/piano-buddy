a55b2dbf040cee9b816559d32d1d1111d2a0177d
Nastaran Moghadasi <nastaran.moghadasi@gmail.com>
2023-12-30 10:56:10 +0100

    get port from env

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/js', express.static(__dirname + '/public/js'));
 app.use('/images', express.static(__dirname + '/public/images'));
 
-const server = app.listen(8081, function(){
+const server = app.listen(process.env.PORT || 8081, function(){
     var port = server.address().port;
     console.log("Server started at http://localhost:%s", port);
 });
